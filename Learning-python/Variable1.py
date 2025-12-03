429b1dedaa3c883908b369523305a986157c9f22
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-04 00:12:08 +0530

    commit

---
+++ Learning-python/Variable1.py
@@ -0,0 +1,35 @@
+## what is Variable?
+#=> it is just like a container , that store some values.
+#It occupies some space in memory.
+#EXAMPLE: a, b, my_variable etc.
+#Water Bottle => just like container , it contains some quantity of water .
+#, Maths
+#x = 10
+#x = Variable and 10 is the value
+## there are some rules to declare a Variable.
+
+#1. Variable name can contain letters, numbers and underscore.
+
+#Ex: a, name, a_b, name_1, name_2 etc.
+
+#2. Variable name must starts with a letter or underscore.
+
+#Ex:
+
+value_1 = 17  # => Wrong
+
+Value_33 = 777   #=> right
+
+_first_name = "SHarik"# => rigte
+
+first_1_name = "WIlmot" #=> right
+
+#3. Variable name are case-sensitive.
+
+##EX:
+
+first_student_name ="Wilmot"
+
+FIRST_student_name = "SHarik"
+
+print(FIRST_student_name)
