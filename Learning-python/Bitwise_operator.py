9c6c62124a3ccf50ee78f34d21509e0c903ad344
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-04 11:45:45 +0530

    operator 5

---
+++ Learning-python/Bitwise_operator.py
@@ -0,0 +1,30 @@
+#5. Bitwise Operations in Python:-
+# => Bitwise operations are used to perform operations on binary numbers.
+# => it works on bits and performs bit by bit operation.
+
+# EX: &, |, ^, ~, 
+
+# AND(&) Operator:-
+# It returns 1 if both the bits are 1. 
+a=5
+b=3
+print("The value of a&b is:",a&b)
+# OR oprator
+A = 5
+B = 6
+print(A | B)
+
+
+#5 = 0101
+#6 = 0110
+   # 0111
+
+A = 5
+B = 6
+print(A ^ B)
+
+#5 = 0101
+#6 = 0110
+   # 0011
+
+
