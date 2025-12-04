e0137c3bc74efed0ceefb22fb983a32d9a00cb47
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-04 10:51:15 +0530

    operator 2

---
+++ Learning-python/Assignment_operator.py
@@ -0,0 +1,13 @@
+#2.assignment operators=> used to assign values to a variable.
+ #ex:+=,-=,*=,%=
+#example :
+a=15
+b=5
+a+=b
+a-=b
+a/=b
+a%=b
+print("the value of a is:",a+b)
+print ("the value of a is :",a-b)
+print ("the value of a is :",a/b)
+print ("the value of a is:",a%b)
--- Learning-python/tempCodeRunnerFile.py
+++ Learning-python/tempCodeRunnerFile.py
@@ -1,3 +1,6 @@
-first_name = "Ansh"
-name_1 = "Ujwal"
-print(first_name)
+a=15
+b=5
+a+=b
+print("after adding the value of a is:",a)
+a-=b
+print("after subtracting the value of a is:",a)
