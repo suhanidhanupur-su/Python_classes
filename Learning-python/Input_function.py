892fa9ed1f768ceaa8e8aaceb267feee8c7b5f6a
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-04 12:18:52 +0530

    first commit

---
+++ Learning-python/Input_function.py
@@ -0,0 +1,28 @@
+## input function :-
+#=> it is used to take data from the user.
+
+#NOte: by default it gives us string value.
+#=> that's why we have to specify, data types , whatever we want for output.
+
+
+
+ #a = 10
+ #b = 20
+ #sum = a + b
+ #print("the sum of a and b is:", sum)
+
+first_value = int(input("Enter first value:"))
+second_value = int(input("Enter second value:"))
+sum = first_value + second_value
+print("the sum of first value and second value is:", sum)
+
+
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+full_name = first_name + " " + last_name
+print("Your full name is:", full_name)
+
+# integer = 10
+# float = 10.5
+# string = "Hello"
+# boolean = True
