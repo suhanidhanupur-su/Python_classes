34c366b1dca7d385d3fbabdb1ac0e8a98dd3883e
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-04 11:48:58 +0530

    Operator 6

---
+++ Learning-python/Membership_operator.py
@@ -0,0 +1,17 @@
+# 5. Membership Operator:-
+# => Membership operators are used to test whether a sequence is presented in an object.
+# => it returns True if the sequence is present in the object otherwise it returns False.
+
+
+# EX: in, not in
+# in Operator:-
+# It returns True if the value is found in the specified sequence, otherwise it returns False.
+
+## not in Operator:-
+# It returns True if the value is not found in the specified sequence, otherwise it returns False
+
+Ravi = [1, 2, 3, 4, 5, 6, 7, 8, 9] 
+print(5 in Ravi)  # True
+print(15 not in Ravi) 
+ 
+
