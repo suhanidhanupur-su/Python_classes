ebe3e9bec9ddfe801187f5ed759e4b4271846b83
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-04 10:29:44 +0530

    operator 1

---
+++ Learning-python/Arthmetic_operator.py
@@ -0,0 +1,40 @@
+## Operators:
+ #=> it can perform any specific operation between two values/Operands.
+
+#4 + 5 => it can perform addition 
+#4 * 5 => it can perform multiplication 
+#=> 4 and  5 => are values/Operands 
+#=> +, *, - =>  are the operators
+ 
+#Types of Operators:
+#1. Arithmetic Operators:
+#=> it is used to perform mathematical operations between two values/Operands.
+#Operators: +, -, *, /, %, //, **
+
+#3**6 => 3^6 => 3*3*3*3*3*3 => 729
+#4**2 => 4*4 = 16
+
+
+## Division operator ("/") => it gives us Quient value.
+#=> it includes the decimal part.
+
+#Ex: 3.5, 4.6, 4.1, 2.5 etc. 
+
+## Floor Division ("//") => it gives us Quient value.
+#=> it removes the decimal part .
+
+## Modulus ("%") => it gives us the remainder part .
+
+
+#1.arthematic operators=> Used to perform mathematical operations between two operands.
+#Ex:+,-,*,%,/,//,**
+ #example:
+a=10
+b=12
+print("the output of a+b is:",a+b)
+print("the output of a-b is:",a-b)
+print("the output of a*b is:",a*b)
+print("the output of division is:",a/b)
+print("the value of modulus is :",a%b) 
+print ("the value of floor division is:",a//b)
+print ("the value of expotentition is : ",a**b)
