8315a01458c41b5ac2189c49c6681e9aed8fec83
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-04 12:26:38 +0530

    first commit

---
+++ Learning-python/string_con.py
@@ -0,0 +1,23 @@
+#String Concatenation ("+") :-  => it joins two strings
+#first_name = "Suhani" 
+#last_name= "xyz"
+#Suhani XYZ 
+
+first_name = "Suhani"
+last_name= "xyz"
+full_name = first_name + ' ' + last_name
+print("Your full name is:",full_name)
+
+#String Replication ("*"):- it is used to repeate any strings multiple time. 
+
+first_name = "Suhani"
+last_name= "xyz"
+full_name = first_name + ' ' + last_name
+print(first_name)
+print(full_name)
+print(full_name*6) #<-String Replication
+name = "suhani"
+print(name*70)   #<-String Replication
+
+
+
