cf68a4ee059e8711131089e937802c33be1e33ac
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-04 11:18:04 +0530

    Operator 4

---
+++ Learning-python/logical_operator.py
@@ -0,0 +1,22 @@
+# 4. Logical Operators: and, or, not
+
+#and operator
+u = 10
+v = 20
+print(u < v and  u > v)
+# True and False => False
+
+# or operator 
+
+s = 10
+t = 20
+print(s < t or s > t)
+# True or False => True
+
+#not operator => it negates the condition
+m = 10
+n = 20
+print(not(m < n))
+# not(True) => False
+print(not(m > n))
+# not(False) => True
