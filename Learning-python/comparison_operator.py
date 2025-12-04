06c749fd2b440085a12554dae96c9728fcfafdc8
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-04 11:05:49 +0530

    operator 3

---
+++ Learning-python/comparison_operator.py
@@ -0,0 +1,14 @@
+# # 3. Comparison Operators: ==, !=, >, <, >=, <=
+
+p = 10
+q = 20
+C = 20
+print(p == q)  
+print(p == C)
+print(p != q)
+print(p > q)
+print("Hii Suhani plzz confirm this stmt: ",p < q)
+print("hello Suhani plzz confirm this stmt: ",p >= q)
+
+print("Hello Isha plzz confirm this stmt: ",C <= q)
+print ("Hello Isha plzz confirm this stmt: ",C >=q)
