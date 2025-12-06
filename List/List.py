d8f1706d4721f22ed7d0db1dab765881a46dc616
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-06 18:24:08 +0530

    first commit

---
+++ List/List.py
@@ -0,0 +1,21 @@
+#  List => it can store multiple types of data.
+#  Example: integer, string, float, boolean, list
+
+
+# List indexing:
+my_list = [ 'Ravi', 'Sonu', 'Ziyarat', 'Ujwal' ]
+print(my_list)
+print("First element of my_list: ", my_list[0])
+print("Second element of my_list: ", my_list[1])
+print("Third element of my_list: ", my_list[2])
+print("Fourth element of my_list: ", my_list[3])
+
+#Negative Indexing:-
+
+my_list = [ 'Ravi', 'Sonu', 'Ziyarat', 'Ujwal' ]
+print("Last element of my_list: ", my_list[-1])
+print("Second last element of my_list: ", my_list[-2])
+print("Third last element of my_list: ", my_list[-3])
+print("Fourth last element of my_list: ", my_list[-4])
+
+
