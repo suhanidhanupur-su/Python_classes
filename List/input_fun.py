f6834ed31bfb2b11fb5ee9f760b2df025e85fdae
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-06 20:43:59 +0530

    program 1

--- List/input_fun.py
+++ List/input_fun.py
@@ -1,8 +1,10 @@
+# 🧮 Program 1: Check if number is Positive or Negative
 my_number = int(input("Hii, Ujwal! Enter your favorite number: "))
 
 if my_number > 0:
     print("Your favorite number is positive.")
 else:
     print("Your favorite number is negative.")
+
 
 
