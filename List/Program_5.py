eb97171833afc4d377eaa39c42d2089b0546bb44
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-09 22:26:30 +0530

    Program 5

---
+++ List/Program_5.py
@@ -0,0 +1,12 @@
+# 🏫 Program 5: Student Grading System
+your_score = int(input("Enter your score (0-100): "))
+if your_score >= 90:
+    print("Grade: A")
+if your_score >= 65:
+    print("Grade: B")
+if your_score >= 55:
+    print("Grade: C")
+else:
+    print("Grade: F")
+print("Thank you for using the grading system.")
+
