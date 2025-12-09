9fd74e62bff0e69a4c09220a1cf24c4eb48a30bf
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-09 22:36:17 +0530

    Program 8

---
+++ List/Program_8.py
@@ -0,0 +1,14 @@
+# 🏫 Program 3: Chek greatest With Check 3 Condition
+my_1st_num = int(input("enter the first number"))
+my_2nd_num = int(input("enter the second number"))
+my_3rd_num = int(input("enter the third number"))
+
+if my_1st_num > my_2nd_num and my_1st_num > my_3rd_num :
+                   print("first number is greatest")
+
+elif my_2nd_num > my_1st_num and my_2nd_num > my_3rd_num:
+                   print("second number is greatest ")
+
+else:
+                   print("third number is greatest")
+
