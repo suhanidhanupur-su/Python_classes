e9304261c3afa39c07272ff7fe46b28ca67cd51f
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-09 22:31:52 +0530

    program 7

---
+++ List/Program_7.py
@@ -0,0 +1,9 @@
+# 🏫 Program 2: Chek greater
+my_1st_num = int(input("Enter the first number: "))
+my_2nd_num = int(input("Enter the second number: "))
+
+if my_1st_num > my_2nd_num:
+    print("First number is greater")
+else:
+    print("Second number is greater")
+
