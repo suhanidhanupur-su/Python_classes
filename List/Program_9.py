bae4889d364ac960ead7d5dbaadf2c7629cb6b2d
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-09 22:50:16 +0530

    Program 9

---
+++ List/Program_9.py
@@ -0,0 +1,13 @@
+# 🏫 Program 4: Create Login credential by if and else
+username = "suhani@123"
+
+password = "12345678"
+
+if username == "suhani@123" and password == "12345678":
+                   print("login succeessfully")
+elif username == "nisha@123" and  password == "12345678":
+                   print("your username is incrrect ")
+
+else:
+                   print("Invalide Credential")
+
