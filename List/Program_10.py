78c383e8d5b8acda98b4bcb499d25c91977ea3e4
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-09 22:59:49 +0530

    Program 10

---
+++ List/Program_10.py
@@ -0,0 +1,15 @@
+# 🏫 Program 5: Revise If / else / elif
+trafic_light = "yellow"
+
+if trafic_light == "red":
+                   print("please Stop")
+
+elif trafic_light == "yellow":
+                   print("ready to go")
+
+elif trafic_light == "green":
+                   print("go")
+
+else:
+                   print("invalide color")
+
