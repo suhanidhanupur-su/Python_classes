8e21b497f90b24e785066b1b60fd2add50ea2a50
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-09 22:29:35 +0530

    Program 6

---
+++ List/Program_6.py
@@ -0,0 +1,14 @@
+# 🏫 Program 1: Chek Positiv / Negative / zero
+#   x > 0 => Positive number
+#   x < 0 => Negative number
+#   x == 0 => zero
+
+my_num = int(input("enter the number"))
+
+if my_num > 0:
+                   print("it is positve number")
+elif my_num == 0:
+                   print("it is zero")
+else:
+                   print("it is negative number")
+
