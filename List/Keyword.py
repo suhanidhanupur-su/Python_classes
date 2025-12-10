fb4ac958cd127f4222b83cf36d3a07dbc067c82c
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-10 19:33:30 +0530

    practice

---
+++ List/Keyword.py
@@ -0,0 +1,49 @@
+print(len("Suhani"))
+
+print(len([1,2,3,4,5,6,7,8,9]))
+
+print(round(7.3))
+
+print(round(6.42,2))
+
+print (sorted(["a","b","c"]))
+
+print(sorted([1,2,3]))
+
+print(pow(5,2))
+
+
+print(pow(10,5))
+
+first_number = int(input("Enter first number:"))
+
+second_number = int(input("Enter second number:"))
+
+print(first_number + second_number )
+
+
+first_name = input("Enter your first name:")
+
+second_name = input("Enter your second name:")
+
+print(first_name +" "+ second_name)
+
+
+first_name = (input("Enter your first name"))  
+
+second_name = (input("Enter your second name"))
+
+print(first_name +" "+ second_name) 
+
+data = input("Enter")
+print(type(data))
+
+for i in range(4,41,4):
+    print(i)
+for i in range (100):
+  print(i)
+
+ print(tuple([1,2,3]))
+
+ print(bool(12))
+print(bool(0))
