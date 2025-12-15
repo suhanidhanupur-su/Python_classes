6f3f23f628ff1e8e7fade96b1a5696e493111d0e
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-15 19:09:35 +0530

    Day_2

---
+++ List/Day_by_day_notes/Day_2.py
@@ -0,0 +1,18 @@
+Day: 2
+
+3.Desktop Application:-
+=> it runs only on desktop.
+=> it does not require any internet connection.
+=>EXAMPLE: Excel, Power-point, Ms-word, NOtepad.
+
+
+TASK: Create a registration form just like google form
+2. Explore Forntend, Backend, and Database. 
+
+
+3.Desktop Application:-
+=> it runs on only desktop.
+=> it does not require any internet connection.
+=>EXAMPLE: Excel, Power-point, Ms-word, NOtepad.
+
+
