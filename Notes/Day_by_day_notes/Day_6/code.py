c601fac75d565f239eaecf1cf76bee426227a022
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-17 15:14:38 +0530

    Day_6

---
+++ Notes/Day_by_day_notes/Day_6/code.py
@@ -0,0 +1,13 @@
+x = 10
+y = 3
+print("the output of x + y : ", x + y)  # Addition
+print("the output of x - y : ", x - y)  # Subtraction
+print("the output of x * y : ", x * y)  # Multiplication
+print("Hii, Ravi, You can see the value of x and y : ")  
+print("hii, this is Zeyarat")
+
+print("the output of division : ", x / y)  # Division
+print("the output of floor division : ", x // y)  # Floor Division
+print("the output of modulus : ", x % y)  # Modulus
+
+print("the output of exponentiation : ", 2 ** 4)  # Exponentiation              
---
+++ Notes/Day_by_day_notes/Day_6/text.py
@@ -0,0 +1,38 @@
+
+Day: 6
+#operators: 
+# An operator is a symbol that tells the compiler or interpreter to perform specific mathematical, relational or logical operation and produce final result.
+Types:
+1. Arithmetic Operators
+2. Assignment Operators
+3. Comparison Operators
+4. Logical Operators
+5. Bitwise Operators
+6. Identity Operators
+7. Membership Operators
+
+1. Arithmetic Operators:
++ , - , * , / ,//,  % , ** 
+
++ => Addition
+- => Subtraction
+* => Multiplication
+
+/ => Division  => it gives us float value, it gives us Quotient value.
+
+// => Floor Division  => it gives us integer value, it gives us Quotient value.
+
+% => Modulus  => it gives us remainder value.
+
+** => Exponentiation  => it gives us power value.
+
+
+
+
+
+## Data Types:
+int => 10, 20, 30....
+float => 10.5, 20.5, 30.5....
+string => "Hello", "World", "Ravi"
+boolean => True, False
+
