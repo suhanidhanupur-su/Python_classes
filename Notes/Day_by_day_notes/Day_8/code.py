68fdaf18a9e9cbd5f14d8de03e82867284deccd8
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-19 19:15:04 +0530

    Day8

---
+++ Notes/Day_by_day_notes/Day_8/code.py
@@ -0,0 +1,35 @@
+
+## input function :-
+
+a = 10
+b = 20
+sum = a + b
+print("the sum of a and b is:", sum)
+
+first_value = int(input("Enter first value:"))
+second_value = int(input("Enter second value:"))
+sum = first_value + second_value
+print("the sum of first value and second value is:", sum)
+
+
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+full_name = first_name + " " + last_name
+print("Your full name is:", full_name)
+
+# logical operators:
+# OR Operator:-
+ 
+A = 10
+B = 20
+print("the output of this A < B and A == 10 is:", A < B and A == 10)
+print("the output of this A > B and A != B is:", A > B and A != B)
+
+print("the output of this A < B or A == 10 is:", A < B or A == 10)
+
+print(A < B or A > 50)
+# the output of line number 31 is: True
+
+
+
+
