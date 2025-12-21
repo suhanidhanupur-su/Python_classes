f1d5ce19cb39d34ad37585f0e325bdcfd8f3e534
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-21 22:45:24 +0530

    Day_12

---
+++ Notes/Day_by_day_notes/class_12/code.py
@@ -0,0 +1,23 @@
+my_string = "    hello world "
+print("Original String: '", my_string)
+print("After using strip(): '", my_string.strip())
+# ---------------------------------------------------
+# List indexing:
+
+
+my_list = [ 'Ravi', 'Sonu', 'Ziyarat', 'Ujwal' ]
+print(my_list)
+print("First element of my_list: ", my_list[0])
+print("Second element of my_list: ", my_list[1])
+print("Third element of my_list: ", my_list[2])
+print("Fourth element of my_list: ", my_list[3])
+# -----------------------------------------------------
+# Negative Indexing:-
+
+my_list = [ 'Ravi', 'Sonu', 'Ziyarat', 'Ujwal' ]
+print("Last element of my_list: ", my_list[-1])
+print("Second last element of my_list: ", my_list[-2])
+print("Third last element of my_list: ", my_list[-3])
+print("Fourth last element of my_list: ", my_list[-4])
+
+# -------------------------------------------------------
---
+++ Notes/Day_by_day_notes/class_12/text.py
@@ -0,0 +1,35 @@
+Day: 12
+
+String Methods in Python
+
+Group of characters./ group of words => String 
+
+-> A string is a sequence of characters enclosed within single quotes (' '), double quotes (" "), or triple quotes (''' ''' or """ """).
+
+
+String methods: -
+
+len() => to find length of string
+lower() => to convert string to lowercase
+upper() => to convert string to uppercase
+title() => to convert first character of each word to uppercase
+strip() => to remove leading and trailing  extra spceces
+split() => to split a string into a list of substrings
+replace() => to replace a substring with another substring
+capitalize() => to capitalize the first character of a string
+find() => to find the index of a substring within a string (returns -1 if not found)
+count() => to count occurrences of a substring in a string
+index() => to find the index of a substring (raises error if not found)
+
+Example
+# --------------------------------------------------------------------------------------------------
+
+ List => [ 'a', 'b', 'c' ]
+ listt = [ 'a', 'b', 'c' ]
+ listt2 = [ 3, 'e', 'Ravi' ]
+ listt3 = [ 1 , 'Ravi', 2.5, True ]
+ listt4 = [ [1,2,3], [ 'a', 'b', 'c' ], [ True, False ] ]
+ List => it can store multiple types of data.
+ Example: integer, string, float, boolean, list
+
+
