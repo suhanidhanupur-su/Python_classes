9c6d0b61490263d7dff3da98e0fe39ead763bcb9
Suhani kumari <suhanidhanupur@gmail.com>
2025-12-27 22:03:15 +0530

    first commit

---
+++ Learning-python/texting.py
@@ -0,0 +1,17 @@
+# class employee:
+#     emp_name = "suhani"
+    
+#     def emp_info (self):
+#         print("Here is details of the  employee")
+
+# emp_obj = employee()
+# print(emp_obj.emp_name)
+#----------------------------------------------------------
+# class employee:
+#     emp_name = "Nisha"
+    
+#     def emp_info (self):
+#         print("Here is details of the  employee")
+
+# emp_obj = employee()
+# print(emp_obj.emp_name)
