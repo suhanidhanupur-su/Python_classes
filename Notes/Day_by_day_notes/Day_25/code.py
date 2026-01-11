1cdd6d4045fa586f9860d4939e84d113a9ab9b6c
Suhani kumari <suhanidhanupur@gmail.com>
2026-01-11 17:59:48 +0530

    commit

---
+++ Notes/Day_by_day_notes/Day_25/code.py
@@ -0,0 +1,26 @@
+def ujwal_function():
+    print("Hii Ujwal! how are u")
+    print("This is Python programming language")
+    print("This is Java Programming Language")
+    print("C++ is Programming language")
+
+ujwal_function()
+# -----------------------------------------------------
+a = 3
+print(a ** 2)
+# ------------------------------------------------------
+def print_sum_of_numbers(a, b):
+    sum = a + b
+    print("Hii Ravi!, here is the sum of a & b:", sum)
+
+print_sum_of_numbers(22, 5)
+
+print("starting")
+print("running")
+
+print_sum_of_numbers(99, 1)
+
+my_variable = 10
+print(my_variable)
+
+
---
+++ Notes/Day_by_day_notes/Day_25/text.py
@@ -0,0 +1,59 @@
+# Day: 29
+# Function Definition
+# A function is created using the keyword def
+# Syntax:
+# def function_name():
+#     # execute some task
+
+
+
+
+# Function Call
+# After defining, we must call the function to run it:
+# def ujwal_function():   #"ujwal_function" this is function name
+#     print("Hii Ujwal! how are u") #Function body performs the task.
+
+
+# ujwal_function() # call function
+
+# Output:
+# Hii Ujwal! how are u
+
+
+
+# Function with Multiple Statements
+# code=
+
+# Output:
+# Hii Ujwal! how are u
+# This is Python programming language
+# This is Java Programming Language
+# C++ is Programming language
+
+
+
+# Global & Local Variables
+# Example: Only Global Variable
+# a = 3
+# print(a ** 2)  # a^2 = 3^2 = 9
+
+# Output:
+# 9
+
+
+
+# Example: Local + Function Calling Multiple Times
+# code=
+
+# Output:
+# Hii Ravi!, here is the sum of a & b: 27
+# starting
+# running
+# Hii Ravi!, here is the sum of a & b: 100
+# 10
+
+
+
+# 📌 Note:
+# sum is local variable → exists only inside function
+# my_variable is global variable → can be used anywhere
