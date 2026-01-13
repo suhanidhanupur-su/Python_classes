4f48b1b758e51f1776bbccc52811f380bc26965e
Suhani kumari <suhanidhanupur@gmail.com>
2026-01-13 22:32:55 +0530

    first commit

---
+++ Learning-python/practice.py
@@ -0,0 +1,47 @@
+ for i in range(10):
+  print(i)
+#  =================
+ for i in  range(5):
+  print(i)
+#  =================
+ for i in range(2,22,2):
+     print(i)
+# =======================
+ for i in range(3,33,3):
+     print(i)
+# =======================
+ for i in range(5,55,5):
+     print(i)
+# ========================
+ my_listt = ["suhani","nisha",88,77,9.7]
+ for i in my_listt:
+    print(i)
+#  ============================================
+my_string = " Suhani kumari  "
+ print(my_string.upper())
+# =========================
+ my_listt = [ "suhani"]
+ print(my_listt.lower())
+# #========================
+ my_listt = [33,55,6.8,32,"Ram","suhani"]
+ for i in my_listt:
+     print("Print my list:",i) 
+#=============================================
+ my_name = "suhani kumari"
+ print("Print my name:", my_name.find("u"))  
+# ===============================================
+ my_string =  "radhe radhe"
+ print(my_string.count("r"))
+# ===================================
+ my_variable = "Suhani"
+ for i in my_variable:
+     print(i)
+# ===================================
+ my_listt = ["suhani", "soni", 22, 45, 57]
+ my_listt.remove(22)
+ print(my_listt)
+# ============================================
+ my_listt = [99,44,33,22,77,55]
+ my_listt.insert(2,9900000)
+ print(my_listt)
+# ================================
--- Learning-python/tempCodeRunnerFile.py
+++ Learning-python/tempCodeRunnerFile.py
@@ -1,6 +1,2 @@
-a=15
-b=5
-a+=b
-print("after adding the value of a is:",a)
-a-=b
-print("after subtracting the value of a is:",a)
+for i in range(10):
+  print(i)
